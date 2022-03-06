a3b47e2f40ac5d78bdfb696d37fed8d5325c1b20
Aditya Trivedi <adityakuldeeptrivedi@gmail.com>
2022-03-06 15:38:11 +0530

    clean code

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,45 @@
-
-import React from 'react';
+import React from "react";
 //import {BrowserRouter as Router ,Switch, Route} from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Formheader from './Formheader';
-import Header from "./Header"
-import Mainbody from './Mainbody';
-import SubmitForm from './QuestionPaper';
-import Question_form from './Question_form';
-import CenteredTabs from './Tabs';
-import Templates from './Templates';
-import User_form from './user_form';
-
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Formheader from "./Formheader";
+import Header from "./Header";
+import SubmitForm from "./QuestionPaper";
+import Question_form from "./Question_form";
+import CenteredTabs from "./Tabs";
+import Templates from "./Templates";
+import User_form from "./user_form";
 
 function App() {
   return (
     <div className="app">
-         <Router>
-         <Routes>
-           
-                   
-           <Route exact path="/form/:id" element={ <><Formheader />  <CenteredTabs /></>}/>
-                 
-              
+      <Router>
+        <Routes>
+          <Route
+            exact
+            path="/form/:id"
+            element={
+              <>
+                <Formheader /> <CenteredTabs />
+              </>
+            }
+          />
 
-              <Route exact path="/response" element={<User_form />}/>
-                 
-              
+          <Route exact path="/response" element={<User_form />} />
 
-              <Route exact path="/submitted" element={<SubmitForm />}/>
-                 
-              
+          <Route exact path="/submitted" element={<SubmitForm />} />
 
-                 
-              
-              <Route exact path="/" element={<><Header /><Templates /><Mainbody /></>}/>
-                
-              
-        
-              </Routes> 
-         </Router>
-
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Header />
+                <Templates />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,43 +1,29 @@
-import {  IconButton } from '@material-ui/core'
-import  MenuIcon from '@material-ui/icons/Menu'
-import React from 'react'
-import "./Header.css"
-import formimage from "./forms_2020q4_48dp.png"
-import SearchIcon from "@material-ui/icons/Search";
-import AppsIcon from '@material-ui/icons/Apps';
-import Avatar from '@material-ui/core/Avatar';
-import avatarimage from "./2.jpg"
-import TemporaryDrawer from "./Drawer"
+import { IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React from "react";
+import "./Header.css";
+import Logo from "./Group1.svg";
+import Avatar from "@material-ui/core/Avatar";
+import avatarimage from "./2.jpg";
 
 function Header() {
-    return (
-        <div className="header">
-
-            <div className="header_info">
-             <TemporaryDrawer/>
-
-                <img src={formimage} style={{height:'40px',width:"40px"}} className="form_image" />
-             <div className="info">
-                 Forms
-             </div>
-            </div>
-            <div className="header_search">
-               <IconButton><SearchIcon /></IconButton>
-                <input type="text" placeholder="Search"/>
-            </div>   
-            <div className="header_right">
-                <IconButton style={{margin:"0px"}}>
-                  <AppsIcon style={{fontSize:"22px"}}/> 
-                </IconButton>
-
-                <IconButton>
-                    
-                    <Avatar style={{height:"30px",width:"30px"}} src={avatarimage}/>
-                </IconButton>
-            </div>          
-
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header_info">
+        <img
+          src={Logo}
+          style={{ height: "40px", width: "40px" }}
+          className="form_image"
+        />
+      </div>
+      <div className="titleHeading">Online Examination Portal</div>
+      <div className="header_right">
+        <IconButton>
+          <Avatar style={{ height: "30px", width: "30px" }} src={avatarimage} />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
